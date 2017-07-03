94c397355c6989a29e4cad4fc1dbe3440e53a272
ahmedtehseen <ahmedpisces7@gmail.com>
2017-07-03 19:51:21 +0500

    login/signup authentication & error messages

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/loginForm';
+
+class App extends Component {
+	componentWillMount() {
+		const config = {
+	    apiKey: 'AIzaSyBXy4YqmTqQWaZzHHc46txWdmMvN0fW3I8',
+	    authDomain: 'react-native-manager-app.firebaseapp.com',
+	    databaseURL: 'https://react-native-manager-app.firebaseio.com',
+	    projectId: 'react-native-manager-app',
+	    storageBucket: '',
+	    messagingSenderId: '966667156119'
+	  };
+
+	  firebase.initializeApp(config);
+	}
+
+	render() {
+	const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+		return (
+			<Provider store={store}>
+				<LoginForm />
+			</Provider>
+		);
+	}
+}
+
+export default App;
